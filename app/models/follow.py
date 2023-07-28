b24527bdecba3b3e68474a686cad0654ea0f45fe
Richard <richylee95@gmail.com>
2023-07-28 16:16:16 -0700

    finished follow model

--- app/models/follow.py
+++ app/models/follow.py
@@ -1,22 +1,11 @@
 from .db import db
-from sqlalchmy import relationship
 
 
 
-class Follow(db.Model):
-    __tablename__='follows'
-    id = db.Column(db.Integer,nullable=False, primary_key=True)
-    follower_id  = db.Column(db.Integer,db.ForeignKey("users.id"), nullable=False)
-    followed_id = db.Column(db.Integer,db.ForeignKey("posts.id"), nullable=False)
 
-#Relationship
-
-user = relationship("User", back_populates="likes")
-post = relationship("Post", back_populates="likes")
-
-def to_dict(self):
-    return {
-        "id":self.id,
-        "follower_id":self.follower_id,
-        "followed_id":self.followed_id
-    }
+follow =db.Table(
+    'follows',
+    db.Model.metadata,
+    db.Column('follower_id',db.Integer,db.ForeignKey("users.id"), primary_key=True),
+    db.Column('followed_id',db.Integer,db.ForeignKey("users.id"), primary_key=True)
+)
