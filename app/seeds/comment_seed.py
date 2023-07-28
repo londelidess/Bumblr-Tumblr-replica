69114be2ffc1ccced47ee426052199290669b417
Brandon <vang.brandon94@gmail.com>
2023-07-28 15:41:34 -0700

    Finished Comment Model

---
+++ app/seeds/comment_seed.py
@@ -0,0 +1,107 @@
+from app.models import db, Comment
+from sqlalchemy.sql import text
+
+
+def seed_comment():
+
+    post1 = Comment(
+        user_id= 1,
+        post_id= 1,
+        content= "Lorem ipsum dolor sit amet, consectetur adipisicing elit. Eligendi non quis exercitationem culpa nesciunt nihil aut nostrum explicabo reprehenderit optio amet ab temporibus asperiores quasi cupiditate. Voluptatum ducimus voluptates voluptas?",
+    )
+
+    post2 = Comment(
+        user_id= 1,
+        post_id= 2,
+        content="Lorem ipsum dolor sit amet, consectetur adipisicing elit. Eligendi non quis exercitationem culpa nesciunt nihil aut nostrum explicabo reprehenderit optio amet ab temporibus asperiores quasi cupiditate. Voluptatum ducimus voluptates voluptas?",
+    )
+
+    post3 = Comment(
+        user_id= 2,
+        post_id= 1,
+        content="Test test test test",
+    )
+
+    post4 = Comment(
+        user_id=2,
+        post_id=2,
+        content="test testes jlkasjdaslkjdlkqjwlekjqwklejqwklejqwklejqlkwj"
+    )
+
+    post5 = Comment(
+        user_id= 3,
+        post_id= 1,
+        content="Lorem ipsum dolor sit amet, consectetur adipisicing elit. Eligendi non quis exercitationem culpa nesciunt nihil aut nostrum explicabo reprehenderit optio amet ab temporibus asperiores quasi cupiditate. Voluptatum ducimus voluptates voluptas?",
+    )
+
+    post6 = Comment(
+        user_id= 3,
+        post_id= 2,
+        content="Lorem ipsum dolor sit amet, consectetur adipisicing elit. Eligendi non quis exercitationem culpa nesciunt nihil aut nostrum explicabo reprehenderit optio amet ab temporibus asperiores quasi cupiditate. Voluptatum ducimus voluptates voluptas?",
+    )
+
+    post7= Comment(
+        user_id= 1,
+        post_id= 3,
+        content= "Lorem ipsum dolor sit amet, consectetur adipisicing elit. Eligendi non quis exercitationem culpa nesciunt nihil aut nostrum explicabo reprehenderit optio amet ab temporibus asperiores quasi cupiditate. Voluptatum ducimus voluptates voluptas?",
+    )
+
+    post8 = Comment(
+        user_id= 1,
+        post_id= 4,
+        content= "Lorem ipsum dolor sit amet, consectetur adipisicing elit. Eligendi non quis exercitationem culpa nesciunt nihil aut nostrum explicabo reprehenderit optio amet ab temporibus asperiores quasi cupiditate. Voluptatum ducimus voluptates voluptas?",
+    )
+
+    post9 = Comment(
+        user_id= 1,
+        post_id= 5,
+        content= "Lorem ipsum dolor sit amet, consectetur adipisicing elit. Eligendi non quis exercitationem culpa nesciunt nihil aut nostrum explicabo reprehenderit optio amet ab temporibus asperiores quasi cupiditate. Voluptatum ducimus voluptates voluptas?",
+    )
+
+    post10 = Comment(
+        user_id= 2,
+        post_id= 3,
+        content= "Lorem ipsum dolor sit amet, consectetur adipisicing elit. Eligendi non quis exercitationem culpa nesciunt nihil aut nostrum explicabo reprehenderit optio amet ab temporibus asperiores quasi cupiditate. Voluptatum ducimus voluptates voluptas?",
+    )
+
+    post11 = Comment(
+        user_id= 2,
+        post_id= 4,
+        content= "Lorem ipsum dolor sit amet, consectetur adipisicing elit. Eligendi non quis exercitationem culpa nesciunt nihil aut nostrum explicabo reprehenderit optio amet ab temporibus asperiores quasi cupiditate. Voluptatum ducimus voluptates voluptas?",
+    )
+
+    post12 = Comment(
+        user_id= 2,
+        post_id= 5,
+        content= "Lorem ipsum dolor sit amet, consectetur adipisicing elit. Eligendi non quis exercitationem culpa nesciunt nihil aut nostrum explicabo reprehenderit optio amet ab temporibus asperiores quasi cupiditate. Voluptatum ducimus voluptates voluptas?",
+    )
+
+    post13 = Comment(
+        user_id= 3,
+        post_id= 3,
+        content= "Lorem ipsum dolor sit amet, consectetur adipisicing elit. Eligendi non quis exercitationem culpa nesciunt nihil aut nostrum explicabo reprehenderit optio amet ab temporibus asperiores quasi cupiditate. Voluptatum ducimus voluptates voluptas?",
+    )
+
+    post14 = Comment(
+        user_id= 3,
+        post_id= 4,
+        content= "Lorem ipsum dolor sit amet, consectetur adipisicing elit. Eligendi non quis exercitationem culpa nesciunt nihil aut nostrum explicabo reprehenderit optio amet ab temporibus asperiores quasi cupiditate. Voluptatum ducimus voluptates voluptas?",
+    )
+
+    post15 = Comment(
+        user_id= 3,
+        post_id= 5,
+        content= "Lorem ipsum dolor sit amet, consectetur adipisicing elit. Eligendi non quis exercitationem culpa nesciunt nihil aut nostrum explicabo reprehenderit optio amet ab temporibus asperiores quasi cupiditate. Voluptatum ducimus voluptates voluptas?",
+    )
+
+
+    all_comments = [post1, post2, post3, post4, post5, post6, post7, post8, post9, post10, post11, post12, post13, post14, post15]
+    add_comments = [db.session.add(comment) for comment in all_comments]
+
+    db.session.commit()
+    return all_comments
+
+
+def undo_comments():
+    db.session.execute(text("DELETE FROM comments"))
+    db.session.commit()
