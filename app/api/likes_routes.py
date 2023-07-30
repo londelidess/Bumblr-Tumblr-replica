593100c48337e35c168314626683c5fcd5134595
Richard <richylee95@gmail.com>
2023-07-30 14:55:35 -0700

    added likes init

--- app/api/likes_routes.py
+++ app/api/likes_routes.py
@@ -4,20 +4,20 @@ from auth_routes import authenticate
 from flask_login import login_required,current_user
 like_routes = Blueprint('likes', __name__)
 
-@like_routes.route('/post/likes/<int:post_id>',methods=['GET'])
+@like_routes.route('/<int:post_id>',methods=['GET'])
 def get_post_likes(post_id):
     post_likes = Like.query.filter_by(post_id=post_id).all()
     response_post_likes=[like.to_dict() for like in post_likes]
     return {"likes":response_post_likes}
 
 
-@like_routes.route('/user/likes/<int:user_id>',methods=['GET'])
+@like_routes.route('/<int:user_id>',methods=['GET'])
 def get_user_likes():
     user_likes = Like.query.filter_by(user_id=current_user.id).all()
     response_user_likes=[like.to_dict() for like in user_likes]
     return {"likes":response_user_likes}
 
-@like_routes.route('/posts/<int:postid>/likes',methods=['POST'])
+@like_routes.route('/<int:postid>/likes',methods=['POST'])
 @login_required
 def add_like(post_id):
     auth = authenticate()
@@ -37,10 +37,10 @@ def add_like(post_id):
     db.session.commit()
     return new_like.to_dict()
 
-@like_routes.route('/posts/<int:postid>/likes',methods=['DELETE'])
+@like_routes.route('/likes/<int:likeid>',methods=['DELETE'])
 @login_required
-def remove_like(id):
-    like_delete = Like.query.get(id)
+def remove_like(likeid):
+    like_delete = Like.query.get(likeid)
     if like_delete is None:
         return jsonify({'error': 'Like not found'}), 404
     
