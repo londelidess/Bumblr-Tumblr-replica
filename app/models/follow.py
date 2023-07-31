63591cbca26a5de2af14dcfef10bb352b8d759a3
Brandon <vang.brandon94@gmail.com>
2023-07-31 12:37:11 -0700

    Added_prefix to follow

--- app/models/follow.py
+++ app/models/follow.py
@@ -1,13 +1,13 @@
 from .db import db, environment, SCHEMA
-
+from db import add_prefix_for_prod
 
 
 
 follow =db.Table(
     'follows',
     db.Model.metadata,
-    db.Column('follower_id',db.Integer,db.ForeignKey("users.id"), primary_key=True),
-    db.Column('followed_id',db.Integer,db.ForeignKey("users.id"), primary_key=True)
+    db.Column('follower_id',db.Integer,db.ForeignKey(add_prefix_for_prod('users.id')), primary_key=True),
+    db.Column('followed_id',db.Integer,db.ForeignKey(add_prefix_for_prod('users.id')), primary_key=True)
 )
 
 if environment == "production":
