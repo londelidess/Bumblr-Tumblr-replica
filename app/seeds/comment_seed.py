04f2e5e114e923c3daa8fd85d14e2369ee21dfea
Brandon <vang.brandon94@gmail.com>
2023-07-31 10:56:08 -0700

    fixed comment seeder post_date

--- app/seeds/comment_seed.py
+++ app/seeds/comment_seed.py
@@ -1,7 +1,7 @@
 from app.models import db, Comment, environment, SCHEMA
 from sqlalchemy.sql import text
-from faker import Faker
-fake = Faker()
+# from faker import Faker
+# fake = Faker()
 from datetime import date
 
 
@@ -11,105 +11,105 @@ def seed_comments():
         user_id= 1,
         post_id= 1,
         content= "Lorem ipsum dolor sit amet, consectetur adipisicing elit. Eligendi non quis exercitationem culpa nesciunt nihil aut nostrum explicabo reprehenderit optio amet ab temporibus asperiores quasi cupiditate. Voluptatum ducimus voluptates voluptas?",
-        post_date= fake.date_between(start_date='-1y', end_date='today'),
+        post_date = date.today(),
     )
 
     post2 = Comment(
         user_id= 1,
         post_id= 2,
         content="Lorem ipsum dolor sit amet, consectetur adipisicing elit. Eligendi non quis exercitationem culpa nesciunt nihil aut nostrum explicabo reprehenderit optio amet ab temporibus asperiores quasi cupiditate. Voluptatum ducimus voluptates voluptas?",
-        post_date= fake.date_between(start_date='-1y', end_date='today'),
+        post_date = date.today(),
     )
 
     post3 = Comment(
         user_id= 2,
         post_id= 1,
         content="Test test test test",
-        post_date= fake.date_between(start_date='-1y', end_date='today'),
+        post_date = date.today(),
     )
 
     post4 = Comment(
         user_id=2,
         post_id=2,
         content="test testes jlkasjdaslkjdlkqjwlekjqwklejqwklejqwklejqlkwj",
-        post_date= fake.date_between(start_date='-1y', end_date='today'),
+        post_date = date.today(),
     )
 
     post5 = Comment(
         user_id= 3,
         post_id= 1,
         content="Lorem ipsum dolor sit amet, consectetur adipisicing elit. Eligendi non quis exercitationem culpa nesciunt nihil aut nostrum explicabo reprehenderit optio amet ab temporibus asperiores quasi cupiditate. Voluptatum ducimus voluptates voluptas?",
-        post_date= fake.date_between(start_date='-1y', end_date='today'),
+        post_date = date.today(),
     )
 
     post6 = Comment(
         user_id= 3,
         post_id= 2,
         content="Lorem ipsum dolor sit amet, consectetur adipisicing elit. Eligendi non quis exercitationem culpa nesciunt nihil aut nostrum explicabo reprehenderit optio amet ab temporibus asperiores quasi cupiditate. Voluptatum ducimus voluptates voluptas?",
-        post_date= fake.date_between(start_date='-1y', end_date='today'),
+        post_date = date.today(),
     )
 
     post7= Comment(
         user_id= 1,
         post_id= 3,
         content= "Lorem ipsum dolor sit amet, consectetur adipisicing elit. Eligendi non quis exercitationem culpa nesciunt nihil aut nostrum explicabo reprehenderit optio amet ab temporibus asperiores quasi cupiditate. Voluptatum ducimus voluptates voluptas?",
-        post_date= fake.date_between(start_date='-1y', end_date='today'),
+        post_date = date.today(),
     )
 
     post8 = Comment(
         user_id= 1,
         post_id= 4,
         content= "Lorem ipsum dolor sit amet, consectetur adipisicing elit. Eligendi non quis exercitationem culpa nesciunt nihil aut nostrum explicabo reprehenderit optio amet ab temporibus asperiores quasi cupiditate. Voluptatum ducimus voluptates voluptas?",
-        post_date= fake.date_between(start_date='-1y', end_date='today'),
+        post_date = date.today(),
     )
 
     post9 = Comment(
         user_id= 1,
         post_id= 5,
         content= "Lorem ipsum dolor sit amet, consectetur adipisicing elit. Eligendi non quis exercitationem culpa nesciunt nihil aut nostrum explicabo reprehenderit optio amet ab temporibus asperiores quasi cupiditate. Voluptatum ducimus voluptates voluptas?",
-        post_date= fake.date_between(start_date='-1y', end_date='today'),
+        post_date = date.today(),
     )
 
     post10 = Comment(
         user_id= 2,
         post_id= 3,
         content= "Lorem ipsum dolor sit amet, consectetur adipisicing elit. Eligendi non quis exercitationem culpa nesciunt nihil aut nostrum explicabo reprehenderit optio amet ab temporibus asperiores quasi cupiditate. Voluptatum ducimus voluptates voluptas?",
-        post_date= fake.date_between(start_date='-1y', end_date='today'),
+        post_date = date.today(),
     )
 
     post11 = Comment(
         user_id= 2,
         post_id= 4,
         content= "Lorem ipsum dolor sit amet, consectetur adipisicing elit. Eligendi non quis exercitationem culpa nesciunt nihil aut nostrum explicabo reprehenderit optio amet ab temporibus asperiores quasi cupiditate. Voluptatum ducimus voluptates voluptas?",
-        post_date= fake.date_between(start_date='-1y', end_date='today'),
+        post_date = date.today(),
     )
 
     post12 = Comment(
         user_id= 2,
         post_id= 5,
         content= "Lorem ipsum dolor sit amet, consectetur adipisicing elit. Eligendi non quis exercitationem culpa nesciunt nihil aut nostrum explicabo reprehenderit optio amet ab temporibus asperiores quasi cupiditate. Voluptatum ducimus voluptates voluptas?",
-        post_date= fake.date_between(start_date='-1y', end_date='today'),
+        post_date = date.today(),
     )
 
     post13 = Comment(
         user_id= 3,
         post_id= 3,
         content= "Lorem ipsum dolor sit amet, consectetur adipisicing elit. Eligendi non quis exercitationem culpa nesciunt nihil aut nostrum explicabo reprehenderit optio amet ab temporibus asperiores quasi cupiditate. Voluptatum ducimus voluptates voluptas?",
-        post_date= fake.date_between(start_date='-1y', end_date='today'),
+        post_date = date.today(),
     )
 
     post14 = Comment(
         user_id= 3,
         post_id= 4,
         content= "Lorem ipsum dolor sit amet, consectetur adipisicing elit. Eligendi non quis exercitationem culpa nesciunt nihil aut nostrum explicabo reprehenderit optio amet ab temporibus asperiores quasi cupiditate. Voluptatum ducimus voluptates voluptas?",
-        post_date= fake.date_between(start_date='-1y', end_date='today'),
+        post_date = date.today(),
     )
 
     post15 = Comment(
         user_id= 3,
         post_id= 5,
         content= "Lorem ipsum dolor sit amet, consectetur adipisicing elit. Eligendi non quis exercitationem culpa nesciunt nihil aut nostrum explicabo reprehenderit optio amet ab temporibus asperiores quasi cupiditate. Voluptatum ducimus voluptates voluptas?",
-        post_date= fake.date_between(start_date='-1y', end_date='today'),
+        post_date = date.today(),
     )
 
 
