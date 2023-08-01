b1a8900c24b5d9f3a2d3efc235357a99775199c3
makoto <d.makoto0701@yahoo.com>
2023-08-01 11:31:35 -0700

    delete add-delete follow reducer

--- react-app/src/store/follow.js
+++ react-app/src/store/follow.js
@@ -6,94 +6,101 @@ const REMOVE_FOLLOW = "follows/REMOVE_FOLLOW";
 
 // Action Creators
 const setLoggedInUserFollowing = (users) => ({
-    type: SET_LOGGED_IN_USER_FOLLOWING,
-    users,
-  });
+  type: SET_LOGGED_IN_USER_FOLLOWING,
+  users,
+});
 
-  const setUserFollowing = (users) => ({
-    type: SET_USER_FOLLOWING,
-    users,
-  });
+const setUserFollowing = (users) => ({
+  type: SET_USER_FOLLOWING,
+  users,
+});
 
-  const addFollow = (user) => ({
-    type: ADD_FOLLOW,
-    user,
-  });
+const addFollow = (user) => ({
+  type: ADD_FOLLOW,
+  user,
+});
 
-  const removeFollow = (userId) => ({
-    type: REMOVE_FOLLOW,
-    userId,
-  });
+const removeFollow = (userId) => ({
+  type: REMOVE_FOLLOW,
+  userId,
+});
 
-  export const fetchLoggedInUserFollowing = () => async (dispatch) => {
-    const response = await fetch(`/api/follows/following`);
-    if (response.ok) {
-      const users = await response.json();
-      dispatch(setLoggedInUserFollowing(users));
-    } else {
-      throw new Error('Failed to fetch the users the logged-in user is following.');
-    }
-  };
-
-  export const fetchUserFollowing = (userId) => async (dispatch) => {
-    const response = await fetch(`/api/follows/${userId}/following`);
-    if (response.ok) {
-      const users = await response.json();
-      dispatch(setUserFollowing(users));
-    } else {
-      throw new Error('Failed to fetch the users a specific user is following.');
-    }
-  };
-
-  export const thunkAddFollow = (userId) => async (dispatch) => {
-    const response = await fetch(`/api/follows/${userId}`, { method: "POST" });
-    if (response.ok) {
-      const user = await response.json();
-      dispatch(addFollow(user));
-    } else {
-      throw new Error('Failed to follow the user.');
-    }
-  };
-
-  export const thunkRemoveFollow = (userId) => async (dispatch) => {
-    const response = await fetch(`/api/follows/${userId}`, { method: "DELETE" });
-    if (response.ok) {
-      dispatch(removeFollow(userId));
-    } else {
-      throw new Error('Failed to unfollow the user.');
-    }
-  };
-
-  const initialState = {
-    loggedInUserFollowing: [],
-    userFollowing: [],
-  };
-
-  export default function followsReducer(state = initialState, action) {
-    switch (action.type) {
-      case SET_LOGGED_IN_USER_FOLLOWING:
-        return {
-            ...state,
-            loggedInUserFollowing: action.users
-        };
-      case SET_USER_FOLLOWING:
-        return {
-            ...state,
-            userFollowing: action.users
-        };
-      case ADD_FOLLOW:
-        return {
-          ...state,
-          loggedInUserFollowing: [...state.loggedInUserFollowing, action.user],
-          userFollowing: [...state.userFollowing, action.user],
-        };
-      case REMOVE_FOLLOW:
-        return {
-          ...state,
-          loggedInUserFollowing: state.loggedInUserFollowing.filter(user => user.id !== action.userId),
-          userFollowing: state.userFollowing.filter(user => user.id !== action.userId),
-        };
-      default:
-        return state;
-    }
+export const fetchLoggedInUserFollowing = () => async (dispatch) => {
+  const response = await fetch(`/api/follows/following`);
+  if (response.ok) {
+    const users = await response.json();
+    dispatch(setLoggedInUserFollowing(users));
+  } else {
+    throw new Error(
+      "Failed to fetch the users the logged-in user is following."
+    );
   }
+};
+
+export const fetchUserFollowing = (userId) => async (dispatch) => {
+  const response = await fetch(`/api/follows/${userId}/following`);
+  if (response.ok) {
+    const users = await response.json();
+    dispatch(setUserFollowing(users));
+  } else {
+    throw new Error("Failed to fetch the users a specific user is following.");
+  }
+};
+
+export const thunkAddFollow = (userId) => async (dispatch) => {
+  const response = await fetch(`/api/follows/${userId}`, { method: "POST" });
+  const data = await response.json();
+
+  if (response.ok) {
+    console.log(data.res);
+    dispatch(fetchLoggedInUserFollowing());
+  } else {
+    throw new Error(data.error || "Failed to follow the user.");
+  }
+};
+
+export const thunkRemoveFollow = (userId) => async (dispatch) => {
+  const response = await fetch(`/api/follows/${userId}`, { method: "DELETE" });
+  const data = await response.json();
+
+  if (response.ok) {
+    console.log(data.res);
+    dispatch(fetchLoggedInUserFollowing());
+  } else {
+    throw new Error(data.error || "Failed to unfollow the user.");
+  }
+};
+
+const initialState = {
+  loggedInUserFollowing: [],
+  userFollowing: [],
+};
+
+export default function followsReducer(state = initialState, action) {
+  switch (action.type) {
+    case SET_LOGGED_IN_USER_FOLLOWING:
+      return {
+        ...state,
+        loggedInUserFollowing: action.users,
+      };
+    case SET_USER_FOLLOWING:
+      return {
+        ...state,
+        userFollowing: action.users,
+      };
+    //   case ADD_FOLLOW:
+    //     return {
+    //       ...state,
+    //       loggedInUserFollowing: [...state.loggedInUserFollowing, action.user],
+    //       userFollowing: [...state.userFollowing, action.user],
+    //     };
+    //   case REMOVE_FOLLOW:
+    //     return {
+    //       ...state,
+    //       loggedInUserFollowing: state.loggedInUserFollowing.filter(user => user.id !== action.userId),
+    //       userFollowing: state.userFollowing.filter(user => user.id !== action.userId),
+    //     };
+    default:
+      return state;
+  }
+}
