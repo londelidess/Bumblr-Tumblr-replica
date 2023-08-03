77b2d509b1a654f48109307c29b064e416a86425
ccnuxuji <ccnuxuji.com>
2023-08-03 08:13:08 -0700

    comments feature

--- react-app/src/components/PostIndex/PostIndexItem/index.js
+++ react-app/src/components/PostIndex/PostIndexItem/index.js
@@ -1,9 +1,10 @@
 import { Link, useHistory } from 'react-router-dom';
 import { useState, useEffect } from "react";
 import { useDispatch, useSelector } from 'react-redux';
-import { thunkDeletePostById, fetchFollowingPosts } from '../../../store/post';
+import { thunkDeletePostById, fetchFollowingPosts, fetchAllPosts } from '../../../store/post';
 import { thunkCreateComment, thunkRemoveComment } from '../../../store/comment'
 import { fetchLoggedInUserFollowing, thunkAddFollow, thunkRemoveFollow } from "../../../store/follow";
+import DeleteIcon from "../../IconCollection/DeleteIcon"
 
 import './PostIndexItem.css';
 import stock from '../../../images/stock.png'
@@ -13,8 +14,6 @@ const getPost = (state) => Object.values(state.posts.allPosts);
 const PostIndexItem = ({ post, fromPath }) => {
     const getCurrentUser = (state) => state.session.user;
     const currentUser = useSelector(getCurrentUser);
-    // console.log("current user is:")
-    // console.log(currentUser)
     const history = useHistory();
     const dispatch = useDispatch();
     const allPosts = useSelector(getPost)
@@ -69,10 +68,10 @@ const PostIndexItem = ({ post, fromPath }) => {
         e.preventDefault();
         const comment_tobe_added = { "content": commentContent };
         try {
-            dispatch(thunkCreateComment(post.id, comment_tobe_added)).then(() => {
-                dispatch(fetchLoggedInUserFollowing());
-                dispatch(fetchFollowingPosts())
-            });
+            await dispatch(thunkCreateComment(post.id, comment_tobe_added))
+            await dispatch(fetchLoggedInUserFollowing());
+            await dispatch(fetchFollowingPosts())
+            await dispatch(fetchAllPosts());
         } catch (error) {
             console.log(error)
         }
@@ -82,7 +81,8 @@ const PostIndexItem = ({ post, fromPath }) => {
         try {
             dispatch(thunkRemoveComment(commentId)).then(() => {
                 dispatch(fetchLoggedInUserFollowing());
-                dispatch(fetchFollowingPosts())
+                dispatch(fetchFollowingPosts());
+                dispatch(fetchAllPosts());
             });
         } catch (error) {
             console.log(error)
@@ -90,7 +90,7 @@ const PostIndexItem = ({ post, fromPath }) => {
     }
 
     useEffect(() => {
-        
+
     }, [dispatch]);
 
     return (
@@ -173,17 +173,23 @@ const PostIndexItem = ({ post, fromPath }) => {
                                         <div>
                                             avatar area
                                         </div>
-                                        <form onSubmit={handleCommentSubmit}>
-                                            <div className='comment-form-content'>
-                                                <input
-                                                    type="text"
-                                                    value={commentContent}
-                                                    placeholder='Send something nice'
-                                                    onChange={(e) => setCommentContent(e.target.value)}
-                                                />
-                                            </div>
-                                            <div><button>Reply</button></div>
-                                        </form>
+                                        
+                                        {currentUser &&
+                                            (
+                                                <form onSubmit={handleCommentSubmit}>
+                                                    <div className='comment-form-content'>
+                                                        <input
+                                                            type="text"
+                                                            value={commentContent}
+                                                            placeholder='Send something nice'
+                                                            onChange={(e) => setCommentContent(e.target.value)}
+                                                        />
+                                                    </div>
+                                                    <div><button>Reply</button></div>
+                                                </form>
+                                            )
+                                        }
+
                                     </div>
                                     <div className='scrolldown-right'>
                                         {post.comments.map((item, index) => (
@@ -197,10 +203,10 @@ const PostIndexItem = ({ post, fromPath }) => {
                                                         {item.content}
                                                     </div>
                                                 </div>
-                                                {   currentUser && currentUser.id === item.user_id &&
+                                                {currentUser && currentUser.id === item.user_id &&
                                                     (
-                                                        <div className='comment-delete-button'>
-                                                            <svg xmlns="http://www.w3.org/2000/svg" height="1em" viewBox="0 0 448 512"><path d="M135.2 17.7L128 32H32C14.3 32 0 46.3 0 64S14.3 96 32 96H416c17.7 0 32-14.3 32-32s-14.3-32-32-32H320l-7.2-14.3C307.4 6.8 296.3 0 284.2 0H163.8c-12.1 0-23.2 6.8-28.6 17.7zM416 128H32L53.2 467c1.6 25.3 22.6 45 47.9 45H346.9c25.3 0 46.3-19.7 47.9-45L416 128z" /></svg>
+                                                        <div className='comment-delete-button' onClick={() => handleDeleteComment(item.id)}>
+                                                            <DeleteIcon />
                                                         </div>
                                                     )
                                                 }
