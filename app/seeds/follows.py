cf5581b59cceb3a67db50b1dca80411998c37661
makoto <d.makoto0701@yahoo.com>
2023-08-04 09:15:47 -0700

    new seeds

--- app/seeds/follows.py
+++ app/seeds/follows.py
@@ -6,11 +6,26 @@ def seed_follows():
     user1_id = "Demo"
     user2_id = "marnie"
     user3_id = "bobbie"
+    user4_id = "alice"
+    user5_id = "charlie"
+    user6_id = "david"
+    user7_id = "evelyn"
+    user8_id = "frank"
+    user9_id = "grace"
+    user10_id = "hannah"
 
 
-    user1 = User.query.filter_by(username = user1_id).first()
-    user2 = User.query.filter_by(username = user2_id).first()
-    user3 = User.query.filter_by(username = user3_id).first()
+
+    user1 = User.query.filter_by(username=user1_id).first()
+    user2 = User.query.filter_by(username=user2_id).first()
+    user3 = User.query.filter_by(username=user3_id).first()
+    user4 = User.query.filter_by(username=user4_id).first()
+    user5 = User.query.filter_by(username=user5_id).first()
+    user6 = User.query.filter_by(username=user6_id).first()
+    user7 = User.query.filter_by(username=user7_id).first()
+    user8 = User.query.filter_by(username=user8_id).first()
+    user9 = User.query.filter_by(username=user9_id).first()
+    user10 = User.query.filter_by(username=user10_id).first()
 
 
     user1.following.append(user2)
@@ -19,6 +34,17 @@ def seed_follows():
     user2.following.append(user3)
     user3.following.append(user1)
     user3.following.append(user2)
+    user4.following.append(user5)
+    user4.following.append(user6)
+    user5.following.append(user7)
+    user5.following.append(user8)
+    user6.following.append(user9)
+    user6.following.append(user10)
+    user7.following.append(user8)
+    user7.following.append(user9)
+    user8.following.append(user10)
+    user9.following.append(user4)
+    user10.following.append(user5)
 
     db.session.commit()
 
--- app/seeds/posts.py
+++ app/seeds/posts.py
@@ -18,8 +18,28 @@ def seed_posts():
         content= "Savoring the Flavors of Thailand: A Culinary Adventure", user_id=2, post_date = datetime_formatted)
     post5 = Post(
         content= "The Future of AI: Impact on Everyday Life", user_id=3, post_date = datetime_formatted)
+    post6 = Post(
+        content="Starry Nights: A Deep Dive into the Cosmos", user_id=3, post_date = datetime_formatted)
+    post7 = Post(
+        content="The Art of Minimalism: Living with Less", user_id=4, post_date = datetime_formatted)
+    post8 = Post(
+        content="The Magic of Old Books: A Journey into Literature's Past", user_id=4, post_date = datetime_formatted)
+    post9 = Post(
+        content="Rediscovering Nature: The Beauty of National Parks", user_id=5, post_date = datetime_formatted)
+    post10 = Post(
+        content="The Rhythms of Jazz: A Musical Exploration", user_id=5, post_date = datetime_formatted)
+    post11 = Post(
+        content="The Intricacies of Origami: The Art of Paper Folding", user_id=6, post_date = datetime_formatted)
+    post12 = Post(
+        content="The World of Coffee: From Bean to Brew", user_id=6, post_date = datetime_formatted)
+    post13 = Post(
+        content="The Marvels of Modern Architecture: Iconic Buildings of the 21st Century", user_id=7, post_date = datetime_formatted)
+    post14 = Post(
+        content="The Joy of Painting: Expressing Emotions through Colors", user_id=7, post_date = datetime_formatted)
+    post15 = Post(
+        content="The Digital Revolution: How Technology is Shaping Our Future", user_id=8, post_date = datetime_formatted)
 
-    posts = [post1, post2, post3, post4, post5]
+    posts = [post1, post2, post3, post4, post5, post6, post7, post8, post9, post10, post11, post12, post13, post14, post15]
     add_posts = [db.session.add(post) for post in posts]
 
 
